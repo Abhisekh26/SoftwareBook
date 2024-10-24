58958d53dc0d4416a54fb43cb512195dbfb4784a
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2024-10-24 13:29:20 +0530

    data sanitization and vlidationddd

--- src/models/user.js
+++ src/models/user.js
@@ -3,18 +3,29 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
+    required:true,
+    trim:true
   },
   lastName: {
     type: String,
+    trim:true
   },
   emailID: {
     type: String,
+    unique:true,                            //data sanitization and validation
+    trim:true
   },
   age: {
     type: Number,
+    min:18
   },
   gender: {
     type: String,
+    validate(value){                         //custom validation 
+        if(!["male","female","others"].includes(value)){
+            throw new Error("please enter your gender")
+        }
+    }
   },
 });
 // making a usermodal based on the user schema 
